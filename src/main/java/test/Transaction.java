3a3729795fd3490019a61fe894bd421c671885cd
Bela Ban <belaban@yahoo.com>
2012-03-14 17:59:04 +0100

    Added timing code

--- src/main/java/test/Transaction.java
+++ src/main/java/test/Transaction.java
@@ -1,12 +1,20 @@
 package test;
 
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class Transaction implements Runnable {
 	int threadIndex;
 	public static AtomicInteger doneThreadsAmount = new AtomicInteger(0);
 	public static final float PRINT_PROGRESS_PERCENTAGE = 0.25f; // Print log messages every X% of thread's progress.
     public static final int PRINT=(int)(RunLoadTest.TRANSACTIONS_PER_THREAD * PRINT_PROGRESS_PERCENTAGE);
+
+
+    protected static final AtomicLong total_get_time=new AtomicLong(0),
+      total_put_time=new AtomicLong(0), total_remove_time=new AtomicLong(0); // nanos
+
+    protected static final AtomicInteger num_gets=new AtomicInteger(0), num_puts=new AtomicInteger(0), num_removes=new AtomicInteger(0);
+
 
 	public Transaction(int threadIndex) {
 		this.threadIndex = threadIndex;
@@ -42,6 +50,14 @@ public class Transaction implements Runnable {
             System.out.println("Amount of transactions in one millisecond is " + transactionsInOneMillisecond);
             double tx_per_sec=totalTransactions / (run_time/ 1000.0);
             System.out.println("Transactions / sec: " + tx_per_sec);
+
+
+            double nanos_per_get=total_get_time.get() / num_gets.get();
+            double nanos_per_put=total_put_time.get() / num_puts.get();
+            double nanos_per_rem=total_remove_time.get() / num_removes.get();
+
+            System.out.println(nanos_per_get + " ns / get, " + nanos_per_put + " ns / put, " + nanos_per_rem + " ns / remove");
+
 		}
 		
 	}
@@ -49,20 +65,45 @@ public class Transaction implements Runnable {
 	private static void startTransaction(int index) {
 		byte[] buffer=new byte[5];
 
-        // RunLoadTest.testCache.startBatch();
+
+        long start, diff;
+
+        start=System.nanoTime();
 		RunLoadTest.testCache.get(index);
 		RunLoadTest.testCache.get(index);
+        diff=System.nanoTime() - start;
+        total_get_time.addAndGet(diff);
+        num_gets.addAndGet(2);
+
+        start=System.nanoTime();
 		RunLoadTest.testCache.put(index, buffer);
+        diff=System.nanoTime() - start;
+        num_puts.incrementAndGet();
+        total_put_time.addAndGet(diff);
+
+        start=System.nanoTime();
 		RunLoadTest.testCache.get(index);
 		RunLoadTest.testCache.get(index);
 		RunLoadTest.testCache.get(index);
 		RunLoadTest.testCache.get(index);
+        diff=System.nanoTime() - start;
+        num_gets.addAndGet(4);
+        total_get_time.addAndGet(diff);
+
+        start=System.nanoTime();
 		RunLoadTest.testCache.remove(index);
+        diff=System.nanoTime() - start;
+        num_removes.incrementAndGet();
+        total_remove_time.addAndGet(diff);
+
+        start=System.nanoTime();
 		RunLoadTest.testCache.get(index);
 		RunLoadTest.testCache.get(index);
 		RunLoadTest.testCache.get(index);
 		RunLoadTest.testCache.get(index);
-        // RunLoadTest.testCache.endBatch(true);
+        diff=System.nanoTime() - start;
+        num_gets.addAndGet(4);
+        total_get_time.addAndGet(diff);
 	}
 
 }
